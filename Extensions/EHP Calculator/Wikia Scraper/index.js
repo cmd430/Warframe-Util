9f9cea481095892df5e17d3854c14cb90f8e5a71
Bradley Treweek <cmd430@gmail.com>
2019-04-16 21:56:52 +1200

    change wiki scraper to beable to support extending the warframe data before saving to json

--- Extensions/EHP Calculator/Wikia Scraper/index.js
+++ Extensions/EHP Calculator/Wikia Scraper/index.js
@@ -1,18 +1,16 @@
 const tabletojson = require('tabletojson')
 const fs = require('fs')
+
 tabletojson.convertUrl('https://warframe.fandom.com/wiki/Warframes_Comparison/Stats', {
     useFirstRowForHeadings: true
   },
   function(tablesAsJson) {
-    let wf_index = {
-      warframes: [],
-      rank: {
-        armor: 1,
-        health: 3,
-        shield: 3,
-        energy: 1.5,
-        strength: 1
-      }
+    let rank_multipliers = {
+      armor: 1,
+      health: 3,
+      shield: 3,
+      energy: 1.5,
+      strength: 1
     }
     let unranked = tablesAsJson[0]
     let rank30 = tablesAsJson[1]
@@ -50,12 +48,12 @@ tabletojson.convertUrl('https://warframe.fandom.com/wiki/Warframes_Comparison/St
             "strength": 100
           }
           let overrides = {
-            rank: {}
+            rank_multipliers: {}
           }
-          let armor = (rank30[index].Armor / warframe.Armor === wf_index.rank.armor ? null : rank30[index].Armor / warframe.Armor)
-          let health = (rank30[index].Health / warframe.Health === wf_index.rank.health ? null : rank30[index].Health / warframe.Health)
-          let shield = (rank30[index].Shields / warframe.Shields === wf_index.rank.shield ? null : rank30[index].Shields / warframe.Shields)
-          let energy = (rank30[index].Energy / warframe.Energy === wf_index.rank.energy ? null : rank30[index].Energy / warframe.Energy)
+          let armor = (rank30[index].Armor / warframe.Armor === rank_multipliers.armor ? null : rank30[index].Armor / warframe.Armor)
+          let health = (rank30[index].Health / warframe.Health === rank_multipliers.health ? null : rank30[index].Health / warframe.Health)
+          let shield = (rank30[index].Shields / warframe.Shields === rank_multipliers.shield ? null : rank30[index].Shields / warframe.Shields)
+          let energy = (rank30[index].Energy / warframe.Energy === rank_multipliers.energy ? null : rank30[index].Energy / warframe.Energy)
           let strength
           switch (warframe.Name.toLowerCase()) {
             case "nidus":
@@ -65,35 +63,58 @@ tabletojson.convertUrl('https://warframe.fandom.com/wiki/Warframes_Comparison/St
               strength = null
           }
           if (armor !== null && !isNaN(armor)) {
-            overrides.rank.armor = armor
+            overrides.rank_multipliers.armor = armor
           }
           if (health !== null && !isNaN(health)) {
-            overrides.rank.health = health
+            overrides.rank_multipliers.health = health
           }
           if (shield !== null && !isNaN(shield)) {
-            overrides.rank.shield = shield
+            overrides.rank_multipliers.shield = shield
           }
           if (energy !== null && !isNaN(energy)) {
-            overrides.rank.energy = energy
+            overrides.rank_multipliers.energy = energy
           }
           if (strength !== null && !isNaN(strength)) {
-            overrides.rank.strength = strength
+            overrides.rank_multipliers.strength = strength
           }
-          if (Object.keys(overrides.rank).length !== 0) {
+          if (Object.keys(overrides.rank_multipliers).length !== 0) {
             warframes[warframe.Name.toLowerCase()].overrides = overrides
           }
         }
       }
     })
     Object.keys(warframes).forEach(warframe => {
-      if (warframe !== "name") {
-        fs.mkdir('warframes/', err => {})
-        fs.writeFile('warframes/' + warframe + '.json', JSON.stringify(warframes[warframe], null, 2), err => {
-          if (err) {
-            return console.log(err)
-          }
+      Promise.all([
+        new Promise((resolve, reject) => {
+          fs.access('abilities/' + warframe + '.json', fs.F_OK, err => {
+            if (!err) {
+              warframes[warframe].abilities = require('./abilities/' + warframe + '.json')
+            }
+            return resolve()
+          })
+        }),
+        new Promise((resolve, reject) => {
+          fs.access('overrides/' + warframe + '.json', fs.F_OK, err => {
+            if (!err) {
+              warframes[warframe].overrides = Object.assign({}, warframes[warframe].overrides, require('./overrides/' + warframe + '.json'))
+            }
+            return resolve()
+          })
         })
-      }
+      ])
+      .then(() => {
+        if (warframe !== "name") {
+          fs.mkdir('warframes/', err => {})
+          fs.writeFile('warframes/' + warframe + '.json', JSON.stringify(warframes[warframe], null, 2), err => {
+            if (err) {
+              return Promise.reject(err)
+            }
+          })
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
     })
   }
 )
